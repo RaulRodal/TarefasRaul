a3f9ae06ae78935c3cc373cab9633f5be4dca5d0
rodal <reulrodalsantome@gmail.com>
2023-12-16 02:53:59 +0100

    Antes comentarios

--- app/src/main/java/es/rodal/tarefasraul/ui/home/HomeScreen.kt
+++ app/src/main/java/es/rodal/tarefasraul/ui/home/HomeScreen.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -187,10 +186,9 @@ private fun TarefasList(
                     )
                     Box(
                         modifier = Modifier
-                            .fillMaxWidth()
+                            .fillMaxWidth() // si pongo fillMaxSize sale por encima y queda feo
                             .background(color)
                             .padding(dimensionResource(id = R.dimen.padding_small))
-                            .height(170.dp)
                             .align(Alignment.CenterVertically),
                         contentAlignment = Alignment.Center
                     ) {
